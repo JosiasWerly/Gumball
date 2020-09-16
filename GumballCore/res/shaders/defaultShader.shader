69067fd37c223335f701d6632e980f91dd501599
Josias Werly <josias.werly@gmail.com>
2020-09-16 01:59:43 -0300

    first drawCall class experiment

--- GumballCore/res/shaders/defaultShader.shader
+++ GumballCore/res/shaders/defaultShader.shader
@@ -8,7 +8,9 @@ void main() {
 //this is a comment
 #shader fragment
 #version 330 core
-layout(location = 0)out vec4 color;
+//layout(location = 0)out vec4 color;
+uniform vec4 uColor;
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    //gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = uColor;
 };
--- GumballCore/src/Source.cpp
+++ GumballCore/src/Source.cpp
@@ -1,6 +1,33 @@
 
 #include "Gumball.h"
 
+
+
+
+#include <fstream>
+#include <sstream>
+struct ShaderSource{
+    string vertex, fragment;
+};
+static ShaderSource loadShaderCode(string filePath) {
+    ifstream fileStream(filePath);
+    string line;
+    bool a = false;
+    ShaderSource out;
+    string outString[2];
+    enum eShaderType {
+        none = -1, vertex, fragment
+    } eShType;
+    while (getline(fileStream, line)) {
+        if (line.find("#shader vertex") != string::npos)
+            eShType = eShaderType::vertex;
+        else if (line.find("#shader fragment") != string::npos)
+            eShType = eShaderType::fragment;
+        else if(line.find("//") == string::npos)
+            outString[eShType] += line + "\n";
+    }
+    return { outString[eShaderType::vertex], outString[eShaderType::fragment] };
+}
 static int compileShader(unsigned int ShaderType, const string& code) {
     unsigned int id = glCreateShader(ShaderType);
     const char* src = code.c_str();
@@ -39,226 +66,132 @@ static int createShader(const string& vertexShader, const string& fragmentShader
     glDeleteShader(fs);
     return p;
 }
-
-
-
-#include <fstream>
-#include <sstream>
-struct ShaderSource{
-    string vertex, fragment;
-};
-static ShaderSource loadShaderCode(string filePath) {
-    ifstream fileStream(filePath);
-    string line;
-    bool a = false;
-    ShaderSource out;
-    string outString[2];
-    enum eShaderType {
-        none = -1, vertex, fragment
-    } eShType;
-    while (getline(fileStream, line)) {
-        if (line.find("#shader vertex") != string::npos)
-            eShType = eShaderType::vertex;
-        else if (line.find("#shader fragment") != string::npos)
-            eShType = eShaderType::fragment;
-        else if(line.find("//") == string::npos)
-            outString[eShType] += line + "\n";
-    }
-    return { outString[eShaderType::vertex], outString[eShaderType::fragment] };
+static int loadShaderFromFile(string filePath) {
+    auto shaderCode = loadShaderCode("res/shaders/defaultShader.shader");    
+    return createShader(shaderCode.vertex, shaderCode.fragment);
 }
+
 
 
 int main() {
     Window w;
     w.setup("gumball", 800, 600);
-    
-    
+    auto a = new debugDraw, b = new debugDraw;
+    w.drawcalls.insert(a);
+    w.drawcalls.insert(b);
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };
-    
-    auto shaderCode = loadShaderCode("defaultShader.shader");
-    unsigned int shader = createShader(shaderCode.vertex, shaderCode.fragment);
-    glUseProgram(shader);
+    a->setup(
+        loadShaderFromFile("res/shaders/defaultShader.shader"), 
+        {
+            -.1,  -.1,
+            .1,  -.1,
+            .1, .1,
+            -.1,  .1
+        },
+        { 
+            0, 1, 2,
+            2, 3, 0 
+        }
+    );
+    b->setup(
+        loadShaderFromFile("res/shaders/defaultShader.shader"),
+        {
+            -.4,  -.2,
+            -.4,  -.0,
+            -.2, .0,
+            -.2,  -.2
+        },
+        {
+            0, 1, 2,
+            2, 3, 0
+        }
+        );
 
 
-
-    unsigned int vb = 0, va = 0;
-    glGenVertexArrays(1, &va);
-    glBindVertexArray(va);
-
-    
-    glGenBuffers(1, &vb);
-    glBindBuffer(GL_ARRAY_BUFFER, vb);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-    glEnableVertexAttribArray(0); //this is weird
-    glBindVertexArray(0); // clear the stack ?
-    
-
-    glUseProgram(shader);
+    //clear all the bind/selectionStack
+    glUseProgram(0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
+    //
     while (true){
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        
-        glBindVertexArray(va);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        w.clear();
-
+        w.clear();       
+        w.draw();
+        w.swap();
     }
 
-    glDeleteVertexArrays(1, &va);
-    glDeleteBuffers(1, &vb);
-    glDeleteProgram(shader);
+    
     return 0;
 }
 
 
-
-
-/*
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <glad/glad.h>
-
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
-#include <iostream>
-
-
-static const struct
-{
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-{
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
-};
-
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
-
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
-
-static void error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-int main(void)
-{
-    GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
-
-    glfwSetErrorCallback(error_callback);
-
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-
-    glfwSetKeyCallback(window, key_callback);
-
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
-    glfwSwapInterval(1);
-
-    // NOTE: OpenGL error checks have been omitted for brevity
-
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
-
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
-
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
-
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
-
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-        sizeof(vertices[0]), (void*)0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
-
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        glm::mat4x4 m, p, mvp;
-
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float)height;
-
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        m = glm::mat4x4(1);
-        m = glm::rotate(m, glm::radians((float)glfwGetTime()*10.f), glm::vec3(0.0f, 0.0f, 1.0f));
-        p = glm::ortho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
-        mvp = p * m;
-
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
-}
-*/
+//int main() {
+//    Window w;
+//    w.setup("gumball", 800, 600);
+//    auto d = new debugDraw;
+//    w.drawcalls.insert(d);
+//
+//    float vertices[] = {
+//        -.5,  -.5,
+//        .5,  -.5,
+//        .5, .5,
+//        -.5,  .5
+//    };
+//    unsigned int vIndex[] = {
+//        0, 1, 2,
+//        2, 3, 0
+//    };
+//
+//    auto shaderCode = loadShaderCode("res/shaders/defaultShader.shader");
+//    unsigned int shader = createShader(shaderCode.vertex, shaderCode.fragment);
+//
+//
+//    //bus of data
+//    unsigned int va = 0;
+//    glGenVertexArrays(1, &va);
+//    glBindVertexArray(va);
+//
+//
+//    // bus of data to send
+//    unsigned int vb = 0;
+//    glGenBuffers(1, &vb);
+//    glBindBuffer(GL_ARRAY_BUFFER, vb);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);//size of the data to send
+//
+//    glEnableVertexAttribArray(0);
+//    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);//bind this vbo to vao
+//
+//    unsigned int ve = 0;
+//    glGenBuffers(1, &ve);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ve);
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vIndex), vIndex, GL_STATIC_DRAW); //??
+//
+//
+//    //clear all the bind/selectionStack
+//    glUseProgram(0);
+//    glBindVertexArray(0);
+//    glBindBuffer(GL_ARRAY_BUFFER, 0);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+//    //
+//    while (true) {
+//        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+//        glClear(GL_COLOR_BUFFER_BIT);
+//
+//
+//        glUseProgram(shader);
+//        glUniform4f(glGetUniformLocation(shader, "uColor"), .0, 0.1, 0.2, 1);
+//        glBindVertexArray(va);
+//        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ve);
+//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+//        //endDrawCall
+//
+//        w.clear();
+//
+//    }
+//
+//    glDeleteVertexArrays(1, &va);
+//    glDeleteBuffers(1, &ve);
+//    glDeleteBuffers(1, &vb);
+//    glDeleteProgram(shader);
+//    return 0;
+//}
