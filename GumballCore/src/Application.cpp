adfdfd29241b502d4756310d9cc53178fade37f8
Josias Werly <josias.werly@gmail.com>
2020-12-12 16:24:24 -0300

    memory exp. pre memory mapping tests with dang pointers

--- GumballCore/src/Application.cpp
+++ GumballCore/src/Application.cpp
@@ -1,7 +1,102 @@
 #include "Gumball.hpp"
 
 void processInput(GLFWwindow* window);
+
+
+struct Mem{
+	void* ptr = 0;
+	unsigned int size = 0;
+	string TypeOf = "";
+};
+static std::vector<Mem> memoryTable;
+void peekMemory() {
+	for (auto m : memoryTable) {
+		cout << m.ptr << " " << m.size << " " << m.TypeOf << endl;
+	}
+}
+
+template<class T> struct Dt {
+public:
+	T* value = nullptr;
+	unsigned int refs = 1;
+	Dt(T* init) : value(init){
+		memoryTable.push_back(
+			{
+				this,
+				sizeof(*this),
+				" "
+			}
+		);
+	}
+	~Dt() {
+		for (size_t i = 0; i < memoryTable.size(); i++)
+			if (memoryTable[i].ptr == this)
+				memoryTable.erase(memoryTable.begin()+i);
+		delete value;
+	}
+};
+template<class T> class Var {
+public:
+	typedef Var<T> varT;
+	typedef Dt<T> dtT;
+
+	dtT* dt;
+	Var(T* ptr = nullptr) {
+		dt = new dtT(ptr ? ptr : new T);
+	}
+	Var(varT& Other) {
+		dt = Other.dt;
+		dt->refs++;
+	}
+	~Var() {
+		if (dt && --dt->refs == 0) {
+			delete dt;
+		}
+	}
+
+	varT& operator=(varT &cpy) {
+		if (dt != cpy.dt) {
+			if (dt)
+				dt->refs--;
+			dt = cpy.dt;
+			dt->refs++;
+		}
+		return *this;
+	}
+
+	operator T* () {
+		return dt ? dt->value : nullptr;
+	}
+	T* operator=(T* newValue) {
+		if (dt->value)
+			delete dt->value;
+		dt->value = newValue;
+		return dt->value;
+	}
+	T* operator()() {
+		return dt->value;
+	}
+
+	operator bool() {
+		return dt->value;
+	}
+};
+
+
+
+
+
+
 int main() {
+	Var<int> a, b, c;
+	a = new int(20);
+	b = a;
+	a = b;
+	peekMemory();
+	c = new int(30);
+	c = a;
+	peekMemory();
+
 	window.create("Gumball", 800, 600);
 	assetManager.loadAssets("res/");
 
