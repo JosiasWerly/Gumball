87bde31ff79a212d6456117ae84b869cf9e01994
Josias Werly <josias.werly@gmail.com>
2021-10-07 19:01:00 -0300

    glad resolved

--- GumballCore/Application.cpp
+++ GumballCore/Application.cpp
@@ -1,38 +1,88 @@
+#include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
-    GLFWwindow* window;
+using namespace std;
+int main() {
+	glfwInit();
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	cout << glGetString(GL_VERSION) << endl;
+	glfwMakeContextCurrent(window);
+	
+	unsigned int i;
+	float pos[6] = {
+		-0.5, -0.5,
+		0.0, 0.5,
+		0.5, -0.5
+	};
+	glGenBuffers(1, &i);
+	glBindBuffer(GL_ARRAY_BUFFER, i);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, pos, GL_STATIC_DRAW);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glfwMakeContextCurrent(window);
-    while (!glfwWindowShouldClose(window)){
-        glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.1, 0.2, 0.3);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0, 0.5);
-        glVertex2f(0.5, -0.5);
-        glEnd();
-
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
+
+
+
+//int main(void){
+//    
+//    GLFWwindow* window;
+//
+//    /* Initialize the library */
+//    if (!glfwInit())
+//        return -1;
+//
+//    /* Create a windowed mode window and its OpenGL context */
+//    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//    if (!window)
+//    {
+//        glfwTerminate();
+//        return -1;
+//    }
+//
+//
+//    glfwMakeContextCurrent(window);
+//    while (!glfwWindowShouldClose(window)){
+//        glClear(GL_COLOR_BUFFER_BIT);
+//
+//        glBegin(GL_TRIANGLES);
+//        glColor3f(0.1, 0.2, 0.3);
+//        glVertex2f(-0.5, -0.5);
+//        glVertex2f(0, 0.5);
+//        glVertex2f(0.5, -0.5);
+//        glEnd();
+//
+//
+//        glfwSwapBuffers(window);
+//        glfwPollEvents();
+//    }
+//
+//    glfwTerminate();
+//    return 0;
+//}
